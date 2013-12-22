a430f922d4fdf6bad56e0f83a797ae4c0afd9e09
barbrick <trevor@barbrick.com>
2013-12-22 11:30:55 -0400

    Change root route

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Blog::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'blog#index'
+  root 'name#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
